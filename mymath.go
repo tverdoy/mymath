4b7948e9c65289d66c39b436e9a5eb8304125c54
tverdoy <tverdoy.work@gmail.com>
2023-11-13 23:48:57 +0300

    Add functions

--- mymath.go
+++ mymath.go
@@ -32,3 +32,13 @@ func Abs(x float64) float64 {
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
 }
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
